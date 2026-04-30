a8b4d4e29cc96bca9a337c18cae5b4b320383ddc
DerFette <42933725+Fettsackmitch@users.noreply.github.com>
2026-04-30 22:25:42 +0200

    Add autostart setting (#5)

--- src/HuntAndPeck/Properties/Settings.Designer.cs
+++ src/HuntAndPeck/Properties/Settings.Designer.cs
@@ -70,5 +70,17 @@ namespace HuntAndPeck.Properties {
                 this["DebugHotKeyText"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        public bool Autostart {
+            get {
+                return ((bool)(this["Autostart"]));
+            }
+            set {
+                this["Autostart"] = value;
+            }
+        }
     }
 }
